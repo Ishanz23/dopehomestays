0a0cb6172ee961d7cb0a3555b73326050eb46b48
Sayantan Dey <sayantandey23@gmail.com>
2021-05-20 16:25:35 +0530

    fix: kanding panel css cleaned up

--- components/LandingPanel.tsx
+++ components/LandingPanel.tsx
@@ -1,13 +1,12 @@
 export default function LandingPanel() {
   return (
     <section
-      className='landing-panel bg-landing-cover bg-cover bg-fixed bg-clip-text py-12 gap-12 text-7xl lg:text-8xl tracking-widest flex flex-col text-center items-center justify-evenly rounded-md dark:bg-gray-800'
+      className='landing-panel bg-landing-cover bg-cover bg-fixed bg-clip-text py-12 lg:py-20 text-7xl lg:text-8xl tracking-widest text-center'
       title='Photo by Chris Rosiak'>
       <p className='flex flex-wrap items-center justify-center uppercase font-black dark:text-transparent'>
         The dopest <p className='px-4 tracking-wide uppercase font-black dark:text-green-500'>Homestays</p> are waiting
-        for you!
+        for you! What are you waiting for?
       </p>
-      <p className='uppercase font-black dark:text-transparent'>What are you waiting for?</p>
     </section>
   )
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
 import Link from 'next/link'
 
-import classes from './Navbar.module.css'
-
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
 
@@ -39,48 +37,5 @@ export default function Navbar() {
         </ul>
       </nav>
     </header>
-    // <header className='w-full p-4 shadow-md flex flex-wrap items-center text-green-700 dark:text-green-400'>
-    //   <div className='w-full flex justify-between items-center dark:text-green-400'>
-    //     <Link href='/'>
-    //       <a className='dark:text-green-400 uppercase'>DOPE HOMESTAYS</a>
-    //     </Link>
-    //     <label htmlFor='menu-toggle' className='pointer-cursor md:hidden block'>
-    //       <svg
-    //         xmlns='http://www.w3.org/2000/svg'
-    //         className='h-5 w-7 dark:text-green-400'
-    //         viewBox='0 0 20 20'
-    //         fill='currentColor'>
-    //         <path
-    //           fill-rule='evenodd'
-    //           d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z'
-    //           clip-rule='evenodd'
-    //         />
-    //       </svg>
-    //     </label>
-    //   </div>
-
-    //   {/* <input className='hidden' type='checkbox' id='menu-toggle' /> */}
-
-    //   <div className='hidden md:flex md:items-center md:w-auto w-full' id='menu'>
-    //     <nav>
-    //       <ul className='md:flex items-center justify-between text-base pt-4 md:pt-0'>
-    //         <li>
-    //           <Link href='/homestays'>
-    //             <a className='md:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-indigo-400' href='#'>
-    //               Homestays
-    //             </a>
-    //           </Link>
-    //         </li>
-    //       </ul>
-    //     </nav>
-    //     {/* <a href='#' className='lg:ml-4 flex items-center justify-start lg:mb-0 mb-4 pointer-cursor'>
-    //       <img
-    //         className='rounded-full w-10 h-10 border-2 border-transparent hover:border-indigo-400'
-    //         src='https://pbs.twimg.com/profile_images/1128143121475342337/e8tkhRaz_normal.jpg'
-    //         alt='Andy Leverenz'
-    //       />
-    //     </a> */}
-    //   </div>
-    // </header>
   )
 }
